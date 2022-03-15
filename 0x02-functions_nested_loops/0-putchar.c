a2cfc8605d33d732d233ed491779067fca1004c7
amakoyeMalcolm <99335161+amakoyeMalcolm@users.noreply.github.com>
2022-03-15 19:35:31 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char text[9] = "_putchar";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
+}
